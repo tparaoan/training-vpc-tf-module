df79ca136e13c86015a2bea28420484e4de3a6fe
tparaoan <tiffany.paraoan@cloudreach>
2022-04-20 16:09:51 -0500

    Resource name for subnet has changed

--- vpc.tf
+++ vpc.tf
@@ -21,7 +21,7 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id     = aws_eip.nat_eip.id
   connectivity_type = "public"
-  subnet_id         = aws_subnet.public.id
+  subnet_id         = aws_subnet.public_a.id
 
   tags = {
     Name = "${var.project_name}-nat-gw"
